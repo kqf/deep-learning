5123e4c1743e19bb8ae00c1e52db5be79c7a7d5a
Sasha <q.dev.null@gmail.com>
2018-11-10 17:07:15 +0100

    Fix random seed for deterministic tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import pytest
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 
 
 @pytest.fixture()
 def data():
+    np.random.seed(0)
     iris = load_iris()
     X, y = iris.data, iris.target
-    X_tr, X_te, y_tr, y_te = train_test_split(X, y)
+    X_tr, X_te, y_tr, y_te = train_test_split(X, y, random_state=0)
     return X_tr, X_te, y_tr, y_te
